cdc81a1612c5413d5c9ad85815e4eb8acb90efea
aidb1993 <aidb1993@gmail.com>
2020-03-08 17:34:48 -0300

    added makeProf admin function

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
 import { User } from '../../core/models/user.model';
 import { LoginService } from '../../core/services/login.service';
 
@@ -9,13 +10,17 @@ import { LoginService } from '../../core/services/login.service';
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private loginService: LoginService) { }
-  users: Array<User>
+  constructor(public loginService: LoginService) { }
+  users: Array<User>;
+  profs: any;
 
   ngOnInit(): void {
     this.loginService.getUsers().subscribe(res => {
       this.users = res;
       console.log(this.users);
+    });
+    this.loginService.getProfs().pipe(take(1)).subscribe(res => {
+      this.profs = res;
     });
   }
 
